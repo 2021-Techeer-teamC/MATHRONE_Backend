e91e6d3b45bea6e6382e495b9b326a50f44b2956
OhHongGi <5ghdrl@naver.com>
2022-08-21 11:04:13 +0900

    modify header in the API that used userId

--- src/main/java/mathrone/backend/controller/ProfileController.java
+++ src/main/java/mathrone/backend/controller/ProfileController.java
@@ -1,13 +1,12 @@
 package mathrone.backend.controller;
 
-import mathrone.backend.controller.dto.TokenRequestDto;
-import mathrone.backend.domain.UserInfo;
 import mathrone.backend.domain.UserProfile;
 import mathrone.backend.service.AuthService;
-import mathrone.backend.service.MainPageService;
 import mathrone.backend.service.ProfileService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profile")
@@ -20,13 +19,12 @@ public class ProfileController {
         this.profileService = profileService;
         this.authService = authService;
     }
-    
+
 
     @RequestMapping(value = "/myprofile", method = RequestMethod.GET)
-    public UserProfile getProfile( @RequestHeader String accessToken,
-                                   @RequestHeader String refreshToken
-    ){
-        return profileService.getProfile(authService.getUserIdFromAT(new TokenRequestDto(accessToken,refreshToken)));
+    public UserProfile getProfile(@RequestHeader String accessToken
+    ) {
+        return profileService.getProfile(authService.getUserIdFromAT(accessToken));
     }
 
 
--- src/main/java/mathrone/backend/service/ProfileService.java
+++ src/main/java/mathrone/backend/service/ProfileService.java
@@ -2,19 +2,13 @@ package mathrone.backend.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import lombok.RequiredArgsConstructor;
-import mathrone.backend.controller.dto.TokenRequestDto;
 import mathrone.backend.domain.UserInfo;
 import mathrone.backend.domain.UserProfile;
 import mathrone.backend.domain.UserRank;
-import mathrone.backend.domain.token.LogoutAccessToken;
-import mathrone.backend.repository.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import mathrone.backend.repository.UserInfoRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
-import mathrone.backend.util.TokenProviderUtil;
-import javax.transaction.Transactional;
 
 @Service
 public class ProfileService {
@@ -22,31 +16,36 @@ public class ProfileService {
     private final UserInfoRepository userInfoRepository;
     private final ZSetOperations<String, String> zSetOperations;
 
-    public ProfileService(UserInfoRepository userInfoRepository, RedisTemplate<String, String> redisTemplate) {
-        this.userInfoRepository=userInfoRepository;
+    public ProfileService(UserInfoRepository userInfoRepository,
+        RedisTemplate<String, String> redisTemplate) {
+        this.userInfoRepository = userInfoRepository;
         this.zSetOperations = redisTemplate.opsForZSet();
     }
 
     //userId를 받아와서 전송
-    public UserProfile getProfile(String id){//이 부분 수정이 필요! (현재 userId가 pk가 아닌 id로 되어있어서 임시로 이렇게 찾는 방법으로 해둠)
+    public UserProfile getProfile(
+        String userId) {//이 부분 수정이 필요! (현재 userId가 pk가 아닌 id로 되어있어서 임시로 이렇게 찾는 방법으로 해둠)
 
         //유저 정보 받아오기
-        UserInfo userinfo = userInfoRepository.findById(id);
+        UserInfo userinfo = userInfoRepository.findByUserId(Integer.parseInt(userId));
         //랭크 정보 받아오기
         ObjectNode node = getMyRank(userinfo.getUserId());
 
         //랭크 정보를 DTO에 담기
-        UserRank r = new UserRank(node.findValue("rank").toString(), node.findValue("score").toString(), node.findValue("try").toString());
+        UserRank r = new UserRank(node.findValue("rank").toString(),
+            node.findValue("score").toString(), node.findValue("try").toString());
 
         //최종 Profile 생성
-        UserProfile res = new UserProfile(userinfo.getUserId(), userinfo.getId(), userinfo.getPassword(), userinfo.getProfileImg(),userinfo.getExp(),
-                userinfo.isPremium(),userinfo.getEmail(), userinfo.getPhoneNum(), userinfo.getUserImg(),userinfo.getRole(),r);
+        UserProfile res = new UserProfile(userinfo.getUserId(), userinfo.getId(),
+            userinfo.getPassword(), userinfo.getProfileImg(), userinfo.getExp(),
+            userinfo.isPremium(), userinfo.getEmail(), userinfo.getPhoneNum(),
+            userinfo.getUserImg(), userinfo.getRole(), r);
 
         return res;
     }
 
 
-    public ObjectNode getMyRank(Integer user_id){ // 리더보드에 필요한 나의 rank 조회
+    public ObjectNode getMyRank(Integer user_id) { // 리더보드에 필요한 나의 rank 조회
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode node = mapper.createObjectNode();
         node.put("rank", zSetOperations.reverseRank("test", user_id.toString()) + 1);
@@ -56,11 +55,9 @@ public class ProfileService {
     }
 
 
-    public UserInfo getUserInfo(long userId){
+    public UserInfo getUserInfo(long userId) {
         return userInfoRepository.getById(userId);
     }
-
-
 
 
 }
