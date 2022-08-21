df50842233bd9218653df87327b42a3549d0a906
OhHongGi <5ghdrl@naver.com>
2022-08-21 10:58:54 +0900

    chore: modify the context in user token for using access token

--- src/main/java/mathrone/backend/config/security/CustomUserDetailsService.java
+++ src/main/java/mathrone/backend/config/security/CustomUserDetailsService.java
@@ -8,9 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import mathrone.backend.config.CacheKey;
-import org.springframework.cache.annotation.Cacheable;
-import java.util.List;
 
 /*
     Repository를 통해 database로부터 필요한 user 정보를 가져오는 service
@@ -24,11 +21,10 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
 //    @Cacheable(value = CacheKey.USER, key = "#email", unless = "#result == null")
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserInfo isExist = userRepository.findByEmail(email).orElseThrow( () ->
-                new UsernameNotFoundException("유저를 찾을 수 없습니다. 이메일을 다시 확인해주세요."));
-
+        UserInfo isExist = userRepository.findByEmail(email).orElseThrow(() ->
+            new UsernameNotFoundException("유저를 찾을 수 없습니다. 이메일을 다시 확인해주세요."));
         return User.builder()
-            .username(isExist.getId())
+            .username(String.valueOf(isExist.getUserId()))
             .password(isExist.getPassword())
             .roles("USER")
             .build();
