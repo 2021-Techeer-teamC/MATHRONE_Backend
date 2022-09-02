2a72c5c5f1718ffbb116bf1a45bea80a01702f17
YujeongLEE <dldbwjd24@ewhain.net>
2022-09-03 01:41:22 +0900

    feat : (sns login) email등 유저 정보를 담을 domain

---
+++ src/main/java/mathrone/backend/controller/dto/OauthDTO/GoogleIDToken.java
@@ -0,0 +1,25 @@
+package mathrone.backend.controller.dto.OauthDTO;
+
+public class GoogleIDToken {
+
+    private String iss;
+    private String azp;
+    private String aud;
+    private String sub;
+    private String atHash;
+    private String hd;
+    private String email;
+    private String emailVerified;
+    private String name;
+    private String picture;
+    private String givenName;
+    private String familyName;
+    private String locale;
+    private String iat;
+    private String exp;
+    private String nonce;
+//    private String alg;
+//    private String kid;
+//    private String typ;
+
+}
