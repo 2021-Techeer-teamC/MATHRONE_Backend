181b317df51b7f506caa7d5f9c7ce2f03c530745
YujeongLEE <dldbwjd24@ewhain.net>
2022-09-03 00:43:21 +0900

    feat : (sns login) code into token
    Refresh token 없음

---
+++ src/main/java/mathrone/backend/service/SnsLoginService.java
@@ -0,0 +1,114 @@
+
+package mathrone.backend.service;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import mathrone.backend.controller.dto.OauthDTO.OAuthLoginUtils;
+import mathrone.backend.controller.dto.OauthDTO.RequestTokenDTO;
+import mathrone.backend.controller.dto.OauthDTO.ResponseTokenDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class SnsLoginService {
+
+    //from Non-static method 'getClientId()' cannot be referenced from a static context error
+    // 객체화 (static)되지 않았기 때문에 객체화 부터 시켜주어야 쓸수 있다.
+    private final OAuthLoginUtils oAuthLoginUtils;
+
+    SnsLoginService(OAuthLoginUtils oAuthLoginUtils){
+        this.oAuthLoginUtils = oAuthLoginUtils;
+    }
+
+
+    public ResponseEntity<ResponseTokenDTO> getToken(String code){
+
+        System.out.println("1");
+        System.out.println(code);
+
+        //이렇게 바로 가져오는 방식으로 하고 builder를 사용하면
+        //https://wildeveloperetrain.tistory.com/143 이런 이유로 NULL이된다 ... (value들이)
+//        RestTemplate restTemplate = new RestTemplate();
+//        RequestTokenDTO requestParams = RequestTokenDTO.builder()
+//                    .code(code)
+//                    .grantType("authorization_code")
+//                    .build();
+//        System.out.println("2");
+//
+//        System.out.println("clientID : "+requestParams.getClientId());
+//        System.out.println("clientSecret : "+requestParams.getClientSecret());
+//        System.out.println("redirectURL : "+requestParams.getRedirectUri());
+//        System.out.println("Grantype : "+requestParams.getGrantType());
+//        System.out.println("code : "+requestParams.getCode());
+//        System.out.println("scope : "+requestParams.getScope());
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        RequestTokenDTO requestParams = RequestTokenDTO.builder()
+                .clientId(oAuthLoginUtils.getClientId())
+                .clientSecret(oAuthLoginUtils.getClientSecret())
+                .code(code)
+                .redirectUri(oAuthLoginUtils.getRedirectUri())
+                .grantType("authorization_code")
+                .build();
+
+
+
+
+
+        try{
+            // Http Header 설정
+
+            System.out.println("3");
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            System.out.println("4");
+            HttpEntity<RequestTokenDTO> httpRequestEntity = new HttpEntity<>(requestParams, headers);
+            System.out.println("5");
+            ResponseEntity<String> apiResponseJson = restTemplate.postForEntity("https://accounts.google.com/o/oauth2/token", httpRequestEntity, String.class);
+            System.out.println("6");
+            // ObjectMapper를 통해 String to Object로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            System.out.println("7");
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+            System.out.println("8");
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
+            System.out.println("9");
+            ResponseTokenDTO googleLoginResponse = objectMapper.readValue(apiResponseJson.getBody(), new TypeReference<ResponseTokenDTO>() {});
+
+            System.out.println(objectMapper.readValue(apiResponseJson.getBody(), new TypeReference<ResponseTokenDTO>() {}));
+            System.out.println("10");
+            System.out.println(googleLoginResponse.getAccessToken());
+            System.out.println("11");
+            System.out.println(googleLoginResponse.getRefreshToken());
+            System.out.println("12");
+            System.out.println(googleLoginResponse.getExpiresIn());
+            System.out.println("13");
+            System.out.println(googleLoginResponse.getTokenType());
+            System.out.println("14");
+            System.out.println(googleLoginResponse.getIdToken());
+            System.out.println("15");
+            return ResponseEntity.ok().body(googleLoginResponse);
+
+        }catch(Exception e){
+            System.out.println("error in backend");
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.badRequest().body(null);
+
+        }
+
+
+    }
+
+
+
