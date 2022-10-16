4f1cc1524bb57788d1aca4c615d77436a7274e2e
YujeongLEE <dldbwjd24@ewhain.net>
2022-10-16 20:25:37 +0900

    feat : resType포함 검색

--- src/main/java/mathrone/backend/repository/UserInfoRepository.java
+++ src/main/java/mathrone/backend/repository/UserInfoRepository.java
@@ -13,13 +13,16 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     //@Query(value = "SELECT nickname FROM user_info WHERE user_id=:user_id", nativeQuery = true)
     UserInfo findByUserId(Integer userId);
 
+
     UserInfo findById(String id);
 
+    UserInfo findByEmailAndResType(String email, String resType);
 
 
     // user_id를 통해서 user_nickname 조회
     @Query(value = "SELECT COUNT(*) FROM problem_try WHERE user_id=:userId GROUP BY user_id", nativeQuery = true)
     Long getTryByUserID(int userId);
+
 
 
     boolean existsByEmailAndResType(String email, String resType);
