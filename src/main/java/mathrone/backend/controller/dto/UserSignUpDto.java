402cc70b4e30aa14374e22d4c73dd0307df3faff
YujeongLEE <dldbwjd24@ewhain.net>
2022-10-16 17:31:10 +0900

    feat : user column 가입경로 추가(resType)

--- src/main/java/mathrone/backend/controller/dto/UserSignUpDto.java
+++ src/main/java/mathrone/backend/controller/dto/UserSignUpDto.java
@@ -7,19 +7,25 @@ import mathrone.backend.domain.UserInfo;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class UserSignUpDto {
     private String email;
     private String password;
     private String id;
 
-    public UserInfo toUser(PasswordEncoder passwordEncoder){
+    public UserSignUpDto(String email, String password, String id) {
+        this.email = email;
+        this.password = password;
+        this.id = id;
+    }
+
+    public UserInfo toUser(PasswordEncoder passwordEncoder, String resType){
         return UserInfo.builder()
                 .id(id)
                 .email(email)
                 .password(passwordEncoder.encode(password))
                 .role("ROLE_USER")
+                .resType(resType)
                 .build();
     }
 }
