835c13c7309a33a072628c09ce7b4ae78179e7a1
YujeongLEE <dldbwjd24@ewhain.net>
2022-10-16 23:34:49 +0900

    chore : 정리

--- src/main/java/mathrone/backend/service/SnsLoginService.java
+++ src/main/java/mathrone/backend/service/SnsLoginService.java
@@ -33,26 +33,6 @@ public class SnsLoginService {
 
     public ResponseEntity<ResponseTokenDTO> getToken(String code) {
 
-        System.out.println("1");
-        System.out.println(code);
-
-        //이렇게 바로 가져오는 방식으로 하고 builder를 사용하면
-        //https://wildeveloperetrain.tistory.com/143 이런 이유로 NULL이된다 ... (value들이)
-//        RestTemplate restTemplate = new RestTemplate();
-//        RequestTokenDTO requestParams = RequestTokenDTO.builder()
-//                    .code(code)
-//                    .grantType("authorization_code")
-//                    .build();
-//        System.out.println("2");
-//
-//        System.out.println("clientID : "+requestParams.getClientId());
-//        System.out.println("clientSecret : "+requestParams.getClientSecret());
-//        System.out.println("redirectURL : "+requestParams.getRedirectUri());
-//        System.out.println("Grantype : "+requestParams.getGrantType());
-//        System.out.println("code : "+requestParams.getCode());
-//        System.out.println("scope : "+requestParams.getScope());
-
-
         RestTemplate restTemplate = new RestTemplate();
         RequestTokenDTO requestParams = RequestTokenDTO.builder()
                 .clientId(oAuthLoginUtils.getClientId())
@@ -67,43 +47,28 @@ public class SnsLoginService {
         try {
             // Http Header 설정
 
-            System.out.println("3");
+
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            System.out.println("4");
+
             HttpEntity<RequestTokenDTO> httpRequestEntity = new HttpEntity<>(requestParams, headers);
-            System.out.println("5");
+
             ResponseEntity<String> apiResponseJson = restTemplate.postForEntity("https://accounts.google.com/o/oauth2/token", httpRequestEntity, String.class);
-            System.out.println("6");
 
 
             // ObjectMapper를 통해 String to Object로 변환
             ObjectMapper objectMapper = new ObjectMapper();
 
-            System.out.println("7");
             objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-            System.out.println("8");
+
             objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
-            System.out.println("9");
+
             ResponseTokenDTO googleLoginResponse = objectMapper.readValue(apiResponseJson.getBody(), new TypeReference<ResponseTokenDTO>() {
             });
 
-            System.out.println(apiResponseJson.getBody());
-            System.out.println("10");
-            System.out.println(googleLoginResponse.getAccessToken());
-            System.out.println("11");
-            System.out.println(googleLoginResponse.getRefreshToken());
-            System.out.println("12");
-            System.out.println(googleLoginResponse.getExpiresIn());
-            System.out.println("13");
-            System.out.println(googleLoginResponse.getTokenType());
-            System.out.println("14");
-            System.out.println(googleLoginResponse.getIdToken());
-            System.out.println("15");
             return ResponseEntity.ok().body(googleLoginResponse);
 
         } catch (Exception e) {
-            System.out.println("error in backend");
             e.printStackTrace();
         }
 
@@ -128,12 +93,6 @@ public class SnsLoginService {
         if (resultJson != null) {
             GoogleIDToken userInfoDto = objectMapper.readValue(resultJson, new TypeReference<GoogleIDToken>() {
             });
-
-            System.out.println("google id token ㅂㅏㄷ아내기");
-            System.out.println(resultJson);
-
-            System.out.println("리턴되는 값 ");
-            System.out.println(userInfoDto);
 
             return ResponseEntity.ok().body(userInfoDto);
         } else {
