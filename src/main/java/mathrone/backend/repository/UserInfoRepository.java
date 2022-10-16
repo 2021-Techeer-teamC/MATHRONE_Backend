cc5c2bde795bfac185795a41fa6008d5af726178
YujeongLEE <dldbwjd24@ewhain.net>
2022-10-16 17:47:35 +0900

    feat : repository ) user exist검색 resType포함

--- src/main/java/mathrone/backend/repository/UserInfoRepository.java
+++ src/main/java/mathrone/backend/repository/UserInfoRepository.java
@@ -16,11 +16,15 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     UserInfo findById(String id);
 
 
+
     // user_id를 통해서 user_nickname 조회
     @Query(value = "SELECT COUNT(*) FROM problem_try WHERE user_id=:userId GROUP BY user_id", nativeQuery = true)
     Long getTryByUserID(int userId);
 
-    Optional<UserInfo> findByEmail(String userId);
+
+    boolean existsByEmailAndResType(String email, String resType);
+
+    Optional<UserInfo> findByEmail(String email);//??? 왜
     boolean existsByEmail(String email);
     Optional<Void> deleteByEmail(String email);
 
