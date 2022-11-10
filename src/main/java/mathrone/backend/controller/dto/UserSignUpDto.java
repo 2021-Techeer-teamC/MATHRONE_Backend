65a858999cd03db0fded28c0f8a130ed35ae13b9
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2022-11-10 15:22:15 +0900

    Mr 105 be login refactoring (#4)
    * chore: Add LogoutAccessToken enum
    
    * chore: Move resoleToken method to TokenProviderUtil file
    
    * chore: Modify id to accountId
    
    * chore: Get RefreshToken from request header

--- src/main/java/mathrone/backend/controller/dto/UserSignUpDto.java
+++ src/main/java/mathrone/backend/controller/dto/UserSignUpDto.java
@@ -1,6 +1,5 @@
 package mathrone.backend.controller.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import mathrone.backend.domain.UserInfo;
@@ -9,19 +8,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Getter
 @NoArgsConstructor
 public class UserSignUpDto {
-    private String email;
+    private String accountId;
     private String password;
-    private String id;
+    private String email;   // email에서 id로 바뀜에 따라, 필요한지 여부 토의하기
 
-    public UserSignUpDto(String email, String password, String id) {
+    public UserSignUpDto(String email, String password, String accountId) {
         this.email = email;
         this.password = password;
-        this.id = id;
+        this.accountId = accountId;
     }
 
     public UserInfo toUser(PasswordEncoder passwordEncoder, String resType){
         return UserInfo.builder()
-                .id(id)
+                .accountId(accountId)
                 .email(email)
                 .password(passwordEncoder.encode(password))
                 .role("ROLE_USER")
