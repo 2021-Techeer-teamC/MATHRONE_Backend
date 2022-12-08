7f1f02e9527e00611c1d09731375267390351f72
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-08 21:49:59 +0900

    (feat) : error cherry pick+ exist google account error type

--- src/main/java/mathrone/backend/error/exception/ErrorCode.java
+++ src/main/java/mathrone/backend/error/exception/ErrorCode.java
@@ -14,6 +14,8 @@ public enum ErrorCode {
     ACCOUNT_NOT_FOUND(UNAUTHORIZED, "아이디를 찾을 수 없습니다."),
     ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "같은 아이디가 이미 존재합니다."),
 
+    GOOGLE_ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "이미 해당 구글계정으로 가입이 진행되었습니다."),
+
     // Common
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생하였습니다.")
 
