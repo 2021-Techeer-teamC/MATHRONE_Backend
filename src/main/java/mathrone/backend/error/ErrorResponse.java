1c6271313adcd3e566f820eb04990c308533d1de
OhHongGi <5ghdrl@naver.com>
2022-12-08 18:00:25 +0900

    chore(ErrorResponse): Add Error response for error handling

---
+++ src/main/java/mathrone/backend/error/ErrorResponse.java
@@ -0,0 +1,26 @@
+package mathrone.backend.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import mathrone.backend.error.exception.ErrorCode;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+            .status(errorCode.getHttpStatus())
+            .body(ErrorResponse.builder()
+                .status(errorCode.getHttpStatus().value())
+                .message(errorCode.getDetail())
+                .build()
+            );
+    }}
