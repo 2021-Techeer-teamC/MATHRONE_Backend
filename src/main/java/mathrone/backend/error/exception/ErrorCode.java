654e71e84f294f1a3ff936860deac34e015233c4
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-08 22:19:41 +0900

    feat: (error) google server error type

--- src/main/java/mathrone/backend/error/exception/ErrorCode.java
+++ src/main/java/mathrone/backend/error/exception/ErrorCode.java
@@ -15,6 +15,10 @@ public enum ErrorCode {
     ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "같은 아이디가 이미 존재합니다."),
 
     GOOGLE_ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "이미 해당 구글계정으로 가입이 진행되었습니다."),
+    GOOGLE_ACCOUNT_NOT_FOUND(UNAUTHORIZED, "해당 계정으로 가입이 진행되지 않았습니다. 회원가입을 진행한 후 로그인해주세요."),
+
+    //google server error
+    GOOGLE_SERVER_ERROR(UNAUTHORIZED, "해당 계정으로 가입이 진행되지 않았습니다. 회원가입을 진행한 후 로그인해주세요."),
 
     // Common
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생하였습니다.")
