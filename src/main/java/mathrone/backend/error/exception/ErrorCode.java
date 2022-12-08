3653e8e3606171d6856a5807f67f6bc4941e5f66
OhHongGi <5ghdrl@naver.com>
2022-12-08 17:56:19 +0900

    chore(ErrorCode): Add Error code for error handling

---
+++ src/main/java/mathrone/backend/error/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package mathrone.backend.error.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // user error
+    ACCOUNT_NOT_FOUND(UNAUTHORIZED, "아이디를 찾을 수 없습니다."),
+    ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "같은 아이디가 이미 존재합니다."),
+
+    // Common
+    SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생하였습니다.")
+
+    ;
+
+
+    private final HttpStatus httpStatus;
+
+    private final String detail;
+
+}
