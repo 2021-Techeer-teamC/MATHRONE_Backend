850d728893d6a5186e1989e7df88bde0edf030df
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-08 23:20:50 +0900

    chore: Modify id to accountId

--- src/main/java/mathrone/backend/domain/token/LogoutAccessToken.java
+++ src/main/java/mathrone/backend/domain/token/LogoutAccessToken.java
@@ -3,28 +3,29 @@ package mathrone.backend.domain.token;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import mathrone.backend.config.CacheKey;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 @AllArgsConstructor
 @Getter
-@RedisHash(value = "logoutAccessToken")
+@RedisHash(value = CacheKey.LogoutAccessToken)
 @Builder
 public class LogoutAccessToken {
 
     @Id
     private String id;
 
-    private String email;
+    private String accessId;
 
     @TimeToLive
     private Long expiration;
 
-    public static LogoutAccessToken of(String accessToken, String email, Long remainExpirationMillSecond) {
+    public static LogoutAccessToken of(String accessToken, String accessId, Long remainExpirationMillSecond) {
         return LogoutAccessToken.builder()
                 .id(accessToken)
-                .email(email)
+                .accessId(accessId)
                 .expiration(remainExpirationMillSecond)
                 .build();
     }
