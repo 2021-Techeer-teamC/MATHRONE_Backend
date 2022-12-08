326e5d6e6f3d9123770eed1c4a21892a6cbdd125
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-08 23:21:27 +0900

    chore: Get RefreshToken from request header

--- src/main/java/mathrone/backend/config/security/SecurityConfig.java
+++ src/main/java/mathrone/backend/config/security/SecurityConfig.java
@@ -39,7 +39,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/problem/**",
             "/answer/**",
             "/rank/**",
-            "/profile/**",
             "/oauth/**"
     };
 
--- src/main/java/mathrone/backend/controller/UserController.java
+++ src/main/java/mathrone/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package mathrone.backend.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import mathrone.backend.controller.dto.*;
 import mathrone.backend.domain.UserInfo;
@@ -11,19 +12,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import mathrone.backend.controller.dto.OauthDTO.*;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
+
     private final AuthService authService;
     private final SnsLoginService snsLoginService;
 
     @GetMapping("/delUser")
-    public ResponseEntity<Void> deleteUser(@RequestParam String email){
-        authService.deleteUser(email);
+    public ResponseEntity<Void> deleteUser(@RequestParam String accountId, String resType) {
+        authService.deleteUser(accountId, resType);
         return ResponseEntity.ok().build();
     }
 
@@ -33,36 +34,31 @@ public class UserController {
     }
 
     @GetMapping("/getRefreshList")
-    public ResponseEntity<List<RefreshToken>> getRefreshList(){
+    public ResponseEntity<List<RefreshToken>> getRefreshList() {
         return ResponseEntity.ok(authService.getRefreshList());
     }
 
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TokenDto> login (@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<TokenDto> login(@RequestBody UserRequestDto userRequestDto) {
         return ResponseEntity.ok(authService.login(userRequestDto));
     }
 
     @PostMapping(value = "/logout", headers = {"Content-type=application/json"})
-    public ResponseEntity<Void> logout( @RequestHeader String accessToken,
-        @RequestHeader String refreshToken
-    ){
-        TokenRequestDto tokenRequestDto = new TokenRequestDto(accessToken,refreshToken);
-        authService.logout(tokenRequestDto);
+    public ResponseEntity<Void> logout(HttpServletRequest request
+    ) {
+        authService.logout(request);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping(value = "/signup", headers = {"Content-type=application/json"})
-    public ResponseEntity<UserResponseDto> signUp(@RequestBody UserSignUpDto userSignUpDto){
+    public ResponseEntity<UserResponseDto> signUp(@RequestBody UserSignUpDto userSignUpDto) {
         return ResponseEntity.ok(authService.signup(userSignUpDto));
     }
 
     @PostMapping(value = "/reissue")
-    public ResponseEntity<TokenDto> reissue (@RequestHeader String accessToken,
-        @RequestHeader String refreshToken){
-        TokenRequestDto tokenRequestDto = new TokenRequestDto(accessToken,refreshToken);
-        return ResponseEntity.ok(authService.reissue(tokenRequestDto));
+    public ResponseEntity<TokenDto> reissue(HttpServletRequest request, @RequestHeader String refreshToken) {
+        return ResponseEntity.ok(authService.reissue(request, refreshToken));
     }
-
 
 
     @PostMapping(value = "/oauth/callback/google", headers = {"Content-type=application/json"})
