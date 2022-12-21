79ae773f0fe3d231656c2c300272f07ae9a24bed
YujeongLEE <dldbwjd24@ewhain.net>
2022-12-21 15:04:25 +0900

    feat: error code 추가

--- src/main/java/mathrone/backend/error/exception/ErrorCode.java
+++ src/main/java/mathrone/backend/error/exception/ErrorCode.java
@@ -14,6 +14,16 @@ public enum ErrorCode {
     ACCOUNT_NOT_FOUND(UNAUTHORIZED, "아이디를 찾을 수 없습니다."),
     ACCOUNT_IS_DUPLICATION(UNAUTHORIZED, "같은 아이디가 이미 존재합니다."),
 
+
+    //user login error
+    /*
+    1. 존재하지 않는 계정
+    2. 비밀번호가 일치하지 않는다
+    */
+
+    ACCOUNT_NOT_EXIST(UNAUTHORIZED,"존재하지 않는 계정입니다."),
+    PASSWORD_NOT_CORRECT(UNAUTHORIZED,"비밀번호를 다시 확인해주세요."),
+
     // Common
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 에러가 발생하였습니다.")
 
