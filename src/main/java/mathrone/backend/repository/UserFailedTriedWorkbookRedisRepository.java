5dd0910f29fc406757ba09898623fa986ca3bb15
OhHongGi <5ghdrl@naver.com>
2022-12-26 23:48:28 +0900

    refactor(UserFailedTriedWorkbookRedisRepository): Modify the primary key type

--- src/main/java/mathrone/backend/repository/UserFailedTriedWorkbookRedisRepository.java
+++ src/main/java/mathrone/backend/repository/UserFailedTriedWorkbookRedisRepository.java
@@ -4,6 +4,6 @@ import mathrone.backend.domain.UserFailedTriedWorkbookRedis;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserFailedTriedWorkbookRedisRepository extends
-    CrudRepository<UserFailedTriedWorkbookRedis, String> {
+    CrudRepository<UserFailedTriedWorkbookRedis, Integer> {
 
 }
