89add2b566dbc126b2777e5d1e047d6b507fc370
Hyunwoo Jang <86454635+aswooo@users.noreply.github.com>
2023-01-04 15:01:59 +0900

    (MR-89) RankinPage-Refactoring (#2)
    * feat: change to use userId from token
    
    * feat: change to use username from token
    
    * faet: change mapping URL
    
    * feat : change id to user_name
    
    * feat: login refactoring에 따른 변경

--- src/main/java/mathrone/backend/controller/RankingController.java
+++ src/main/java/mathrone/backend/controller/RankingController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import mathrone.backend.service.RankService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 @RequestMapping("/rank")
@@ -14,18 +15,17 @@ public class RankingController {
         this.rankService = rankService;
     }
 
-    @GetMapping("/rankdata") // 상위 랭킹 정보를 가져옴
+    @GetMapping("/total-rank") // 상위 랭킹 정보를 가져옴
     public ArrayNode getRank(){
         return rankService.getAllRank();
     }
 
-    @GetMapping("/myrankdata") // 나의 랭킹 정보를 가져오기
-    public ObjectNode getMyRank(@RequestParam(value="userId", required = false) Integer userId){  // user_id(int)를 파라미터로 필요로 함
-        return rankService.getMyRank(userId);
+    @GetMapping("/rank") // 나의 랭킹 정보를 가져오기
+    public ObjectNode getMyRank(@RequestHeader String accessToken){  // user_id(int)를 파라미터로 필요로 함
+        return rankService.getMyRank(accessToken);
     }
 
-    @PostMapping("/setdata") // 맞춘 문제에 개수 업데이트하기
-    public void setRank(/*nickname*/){
-        rankService.setRank(/*nickname*/);
-    }
+//    @PostMapping("/setdata") // 맞춘 문제에 개수 업데이트하기
+//    public void setRank(@RequestHeader String accessToken){rankService.setRank(accessToken);
+//    }
 }
