abc1394a971436e834adb576269551d1e8c04604
OhHongGi <5ghdrl@naver.com>
2023-01-04 22:37:03 +0900

    refactor(application): Change setting that GCP-SQL to GCP-VM

--- src/main/java/mathrone/backend/repository/WorkBookRepository.java
+++ src/main/java/mathrone/backend/repository/WorkBookRepository.java
@@ -2,19 +2,14 @@ package mathrone.backend.repository;
 
 import mathrone.backend.domain.PubCatPair;
 import mathrone.backend.domain.WorkBookInfo;
-import org.apache.commons.lang3.tuple.Pair;
+//import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Temporal;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TemporalType;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface WorkBookRepository extends JpaRepository<WorkBookInfo, Long> {
@@ -26,18 +21,17 @@ public interface WorkBookRepository extends JpaRepository<WorkBookInfo, Long> {
     Page<WorkBookInfo> findAllByPublisher(String publisher, Pageable pageable);
 
     //publisher와 category를 이용하여 workbook을 찾는 기능 -> Workbook Info 타입의 리스트를 반환
-    Page<WorkBookInfo> findAllByPublisherAndCategory(String publisher, String category, Pageable pageable);
+    Page<WorkBookInfo> findAllByPublisherAndCategory(String publisher, String category,
+        Pageable pageable);
 
     //결과의 수 반환
     Long countByPublisher(String publisher);
+
     Long countByPublisherAndCategory(String publisher, String category);
 
-    @Query( value = "SELECT publisher, category FROM workbook GROUP BY publisher, category", nativeQuery = true)
+    @Query(value = "SELECT publisher, category FROM workbook GROUP BY publisher, category", nativeQuery = true)
     List<PubCatPair> findGroupByPublisherAndCategory();
-    
-    // publisher에 따라 workbook 전부 조회
-    List<WorkBookInfo> findByPublisher(String publisher);
-  
+
     // workbookId로 해당 workbook 조회
     WorkBookInfo findByWorkbookId(String workbookId);
 }
