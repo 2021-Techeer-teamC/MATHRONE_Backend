090ea0bb8e99deb3231fa9da20da8b5aaf9a6e9c
Hyunwoo Jang <86454635+aswooo@users.noreply.github.com>
2023-05-04 20:56:43 +0900

    (MR-115 )BE 채점 api 업데이트 (#18)
    * feat: allow put method to cors
    
    * feat: add solution in responseData
    
    * feat: add grading only solved problems
    
    * feat: add isAll props in gradeRequestDTO
    
    * feat: 전체 채점, 푼것만 채점 분리
    
    * chore: 자료형 변경 int -> integer
    
    * feat: 문제 채점시 점수 업데이트
    
    * chore: 주석 추가
    
    * chore: update build.gradle

--- src/main/java/mathrone/backend/config/WebConfig.java
+++ src/main/java/mathrone/backend/config/WebConfig.java
@@ -2,6 +2,7 @@ package mathrone.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
