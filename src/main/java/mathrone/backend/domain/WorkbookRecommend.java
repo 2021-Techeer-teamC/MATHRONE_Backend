af384afd0d91eb7745c21ea05262479b0c13f394
OhHongGi <5ghdrl@naver.com>
2023-05-25 02:37:44 +0900

    refactor(WorkbookRecommend): fix the column type
    String -> WorkBookInfo
    
    n:1 relationship between WorkBookRecommend and WorkBookInfo

--- src/main/java/mathrone/backend/domain/WorkbookRecommend.java
+++ src/main/java/mathrone/backend/domain/WorkbookRecommend.java
@@ -1,15 +1,15 @@
 package mathrone.backend.domain;
 
 import com.vladmihalcea.hibernate.type.array.IntArrayType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.TypeDef;
 
 @Getter
@@ -22,8 +22,9 @@ public class WorkbookRecommend {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //JPA 사용시 필요)
     private int id;
 
-    @Column(name = "workbook_id")
-    private String workbookId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workbook_id")
+    private WorkBookInfo workbookId;
 
     private String intro;
 
