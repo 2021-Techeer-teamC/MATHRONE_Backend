c7c77575e533fe93620d3f7b802509fd551fddad
OhHongGi <5ghdrl@naver.com>
2023-05-25 15:42:59 +0900

    docs(WorkbookController): Add description to api
    api
    - /workbook/list
    - /workbook/count
    - /workbook/summary
    - /workbook/try
    - /workbook/star

--- src/main/java/mathrone/backend/controller/WorkbookController.java
+++ src/main/java/mathrone/backend/controller/WorkbookController.java
@@ -1,5 +1,6 @@
 package mathrone.backend.controller;
 
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import mathrone.backend.domain.UserWorkbookData;
@@ -18,16 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/workbook")
 public class WorkbookController {
 
-
     private final WorkBookService workBookService;
 
     public WorkbookController(WorkBookService workBookService) {
         this.workBookService = workBookService;
     }
 
-
-    //workbook API
-    @GetMapping("/list") // 모든 워크북 조회(Books page)
+    @GetMapping("/list")
+    @ApiOperation(value = "workbook 조회", notes = "parameter에 따라 filtering된 workbook 리스트 가져오기")
     public List<bookItem> bookList(
         @RequestParam(value = "publisher", required = false, defaultValue = "all") String publisher,
         @RequestParam(value = "sortType", required = false, defaultValue = "star") String sortType,
@@ -39,7 +38,8 @@ public class WorkbookController {
     }
 
 
-    @GetMapping("/count") // 모든 워크북 조회(Books page)
+    @GetMapping("/count")
+    @ApiOperation(value = "workbook 개수 조회", notes = "parameter에 따라 filtering된 workbook 개수 반환")
     public Long bookCount(
         @RequestParam(value = "publisher", required = false, defaultValue = "all") String publisher,
         @RequestParam(value = "category", required = false, defaultValue = "all") String category) {
@@ -49,17 +49,20 @@ public class WorkbookController {
 
 
     @GetMapping("/summary")
+    @ApiOperation(value = "문제집 리스트 반환", notes = "publisher와 categories로 group화된 workbook 리스트 반환")
     public List<bookContent> workbookList() {
         return workBookService.getWorkbookList();
     }
 
     @GetMapping("/try")
+    @ApiOperation(value = "사용자가 시도한 문제집 리스트 반환", notes = "access token가 존재하면 특정 사용자, 존재하지 않으면 모든 사용자가 시도한 문제집 리스트를 반환")
     public List<UserWorkbookData> getTryingList(
         HttpServletRequest request) {
         return workBookService.getTryingBook(request);
     }
 
     @GetMapping("/star")
+    @ApiOperation(value = "사용자가 즐겨찾는 문제집 리스트 반환", notes = "access token가 존재하면 특정 사용자, 존재하지 않으면 모든 사용자가 즐겨찾는 문제집 리스트를 반환")
     public List<UserWorkbookData> getStarList(
         HttpServletRequest request) {
         return workBookService.getStarBook(request);
