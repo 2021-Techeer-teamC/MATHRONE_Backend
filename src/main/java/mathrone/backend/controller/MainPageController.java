c1318e80bb2360a1e7ee3e902b25504529d306cb
OhHongGi <5ghdrl@naver.com>
2023-05-25 16:38:02 +0900

    docs(MainPageController): Add description to all api

--- src/main/java/mathrone/backend/controller/MainPageController.java
+++ src/main/java/mathrone/backend/controller/MainPageController.java
@@ -1,6 +1,8 @@
 package mathrone.backend.controller;
 
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mathrone.backend.controller.dto.CarouselResponseDto;
 import mathrone.backend.service.MainPageService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/main")
 public class MainPageController {
 
     private final MainPageService mainPageService;
 
-    public MainPageController(MainPageService mainPageService) {
-        this.mainPageService = mainPageService;
-    }
-
     @GetMapping("/carousel/list")
+    @ApiOperation(value = "메인 페이지 workbook 리스트 반환", notes = "메인 페이지에 보여줄 workbook 정보(image, title 등) 가져옴")
     public List<CarouselResponseDto> getCarousel() {
         return mainPageService.getCarousel();
     }
