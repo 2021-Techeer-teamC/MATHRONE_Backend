5245c387151fe7c00df7600b8d07d797ab3cde63
hyunwoo <ashyonu@gmail.com>
2023-06-02 01:38:29 +0900

    feat: add workbookDetail service

--- src/main/java/mathrone/backend/domain/WorkBookInfo.java
+++ src/main/java/mathrone/backend/domain/WorkBookInfo.java
@@ -1,5 +1,7 @@
 package mathrone.backend.domain;
 import com.sun.istack.NotNull;
+import com.vladmihalcea.hibernate.type.array.IntArrayType;
+import com.vladmihalcea.hibernate.type.array.LongArrayType;
 import com.vladmihalcea.hibernate.type.array.StringArrayType;
 import lombok.NoArgsConstructor;
 import mathrone.backend.controller.dto.CarouselResponseDto;
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "workbook")
 @TypeDef(name = "String-array", typeClass = StringArrayType.class)
+@TypeDef(name = "Long-array", typeClass = LongArrayType.class)
 public class WorkBookInfo {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //JPA 사용시 필요
@@ -42,6 +45,9 @@ public class WorkBookInfo {
     @Type(type = "String-array")
     private String[] chapterId;
 
+    @Type(type = "Long-array")
+    private Long[] tags;
+
     private String category;
 
     public String getWorkbookId() {
@@ -66,6 +72,14 @@ public class WorkBookInfo {
 
     public String getType() {
         return type;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public Long[] getTags() {
+        return tags;
     }
 
     public Short getYear() {
