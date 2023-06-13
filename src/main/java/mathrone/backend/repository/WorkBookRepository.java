4391973b46d32215c46167eef9e57f0e9df28170
OhHongGi <5ghdrl@naver.com>
2023-06-13 20:43:38 +0900

    chore(WorkBookRepository): Add some query
    - findByUserSolvedWorkbook
    - findByUserSolvedAllWorkbook

--- src/main/java/mathrone/backend/repository/WorkBookRepository.java
+++ src/main/java/mathrone/backend/repository/WorkBookRepository.java
@@ -1,5 +1,7 @@
 package mathrone.backend.repository;
 
+import java.util.List;
+import mathrone.backend.controller.dto.interfaces.UserSolvedWorkbookResponseDtoInterface;
 import mathrone.backend.domain.PubCatPair;
 import mathrone.backend.domain.WorkBookInfo;
 import org.springframework.data.domain.Page;
@@ -7,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface WorkBookRepository extends JpaRepository<WorkBookInfo, String> {
@@ -33,4 +33,35 @@ public interface WorkBookRepository extends JpaRepository<WorkBookInfo, String> 
 
     // workbookId로 해당 workbook 조회
     WorkBookInfo findByWorkbookId(String workbookId);
+
+    // 유저가 푼 문제집에 대해 문제집 Id, 전체 문제 개수, 푼 문제 개수 반환
+    @Query(value =
+        "SELECT pm.workbook_id as workbookId, pm.tc as totalProblemCount, pt.sc as solvedProblemCount "
+            + "FROM (SELECT workbook_id, count(*) as tc "
+            + "FROM problem "
+            + "WHERE workbook_id = :workbookId "
+            + "GROUP BY workbook_id) as pm, "
+            + "(SELECT LEFT(problem_id, 2) as workbook_id, count(*) as sc "
+            + "FROM problem_try "
+            + "WHERE iscorrect = true "
+            + "AND LEFT(problem_id, 2) = :workbookId "
+            + "AND user_id = :userId "
+            + "GROUP BY LEFT(problem_id, 2)) as pt", nativeQuery = true)
+    List<UserSolvedWorkbookResponseDtoInterface> findByUserSolvedWorkbook(String workbookId,
+        int userId);
+
+    // 유저가 푼 모든 문제집에 대해 문제집 Id, 전체 문제 개수, 푼 문제 개수 반환
+    @Query(value =
+        "SELECT pm.workbook_id as workbookId, pm.tc as totalProblemCount, pt.sc as solvedProblemCount "
+            + "FROM (SELECT workbook_id, count(*) as tc "
+            + "FROM problem "
+            + "GROUP BY workbook_id) as pm, "
+            + "(SELECT LEFT(problem_id, 2) as workbook_id, count(*) as sc "
+            + "FROM problem_try "
+            + "WHERE iscorrect = true "
+            + "AND user_id = :userId "
+            + "GROUP BY LEFT(problem_id, 2)) as pt "
+            + "WHERE pm.workbook_id = pt.workbook_id "
+            + "ORDER BY pm.workbook_id", nativeQuery = true)
+    List<UserSolvedWorkbookResponseDtoInterface> findByUserSolvedAllWorkbook(int userId);
 }
