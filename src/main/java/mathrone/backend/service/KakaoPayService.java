d5e6f4ceeabe33c40efbbe367b557f2199711a9c
YujeongLEE <dldbwjd24@ewhain.net>
2023-07-16 15:28:15 +0900

    feat : 카카오 측에 페이요청 관련 서비스

--- src/main/java/mathrone/backend/service/KakaoPayService.java
+++ src/main/java/mathrone/backend/service/KakaoPayService.java
@@ -6,9 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import mathrone.backend.controller.dto.OauthDTO.Kakao.KakaoTokenResponseDTO;
-import mathrone.backend.domain.kakaoPay.KakaoPaymentOAuthUtils;
-import mathrone.backend.domain.kakaoPay.KakaoPaymentRequest;
-import mathrone.backend.domain.kakaoPay.KakaoPaymentResponse;
+import mathrone.backend.domain.kakaoPay.*;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -92,6 +90,74 @@ public class KakaoPayService {
                     });
 
             return kakaoPaymentResponse;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+
+    }
+
+
+
+    public KakaoPaymentApproveResponse approveForKakaoPayment(KakaoPaymentApproveRequest kakaoPaymentApproveRequest){
+
+        try {
+
+            RestTemplate rt = new RestTemplate();
+            rt.setRequestFactory(
+                    new HttpComponentsClientHttpRequestFactory()); //error message type및 description확인 가능
+
+            // 해더 만들기
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+            headers.add("Authorization", "KakaoAK "+ kakaoPaymentOAuthUtils.getAppAdminKye());
+
+            // 바디 만들기 (HashMap 사용 불가!)
+            MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+            params.add("cid", kakaoPaymentApproveRequest.getCid());
+            params.add("tid", kakaoPaymentApproveRequest.getTid());
+            params.add("partner_order_id", kakaoPaymentApproveRequest.getPartner_order_id());
+            params.add("partner_user_id", kakaoPaymentApproveRequest.getPartner_user_id());
+            params.add("pg_token", kakaoPaymentApproveRequest.getPg_token());
+
+            // 해더와 바디를 하나의 오브젝트로 만들기
+            HttpEntity<MultiValueMap<String, Object>> kakaoTokenRequest =
+                    new HttpEntity<>(params, headers);
+
+            // Http 요청하고 리턴값을 response 변수로 받기
+            ResponseEntity<String> apiResponseJson = rt.exchange(
+                    "https://kapi.kakao.com/v1/payment/approve", // Host
+                    HttpMethod.POST, // Request Method
+                    kakaoTokenRequest,    // RequestBody
+                    String.class
+            );    // return Object
+
+
+            System.out.println(apiResponseJson.toString());
+            System.out.println(apiResponseJson.getBody().toString());
+
+            // ObjectMapper를 통해 String to Object로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            /*
+            com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Java 8 date/time type `java.time.LocalDateTime` not supported by default: add Module "com.fasterxml.jackson.datatype:jackson-datatype-jsr310" to enable handling
+             */
+
+            objectMapper.registerModule(new JavaTimeModule());
+
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+            objectMapper.setSerializationInclusion(
+                    JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
+
+            KakaoPaymentApproveResponse kakaoPaymentApproveResponse = objectMapper.readValue(
+                    apiResponseJson.getBody(), new TypeReference<KakaoPaymentApproveResponse>() {
+                    });
+
+            return kakaoPaymentApproveResponse;
 
         } catch (Exception e) {
             e.printStackTrace();
