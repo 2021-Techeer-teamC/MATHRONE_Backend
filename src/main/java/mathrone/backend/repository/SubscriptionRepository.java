40a6996e50dd41fc3a212f2e2b08270dd8801808
YujeongLEE <dldbwjd24@ewhain.net>
2023-08-16 22:21:24 +0900

    feat : 로그인 시 구독 만료 체크 (카카오,구글, 기본 로그인)

--- src/main/java/mathrone/backend/repository/SubscriptionRepository.java
+++ src/main/java/mathrone/backend/repository/SubscriptionRepository.java
@@ -3,6 +3,7 @@ package mathrone.backend.repository;
 
 import mathrone.backend.domain.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +13,9 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
 
 
     Optional<Subscription> findByTid(String tid);
+
+
+    //구독이 complete 상태이며, 가장 최신의 것
+    @Query(value = "select * from subscription where user_id=:userId and status = 'COMPLETE' order by modified_date desc limit 1", nativeQuery = true)
+    Optional<Subscription> findByUserId(int userId);
 }
