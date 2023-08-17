a1c67445af36ff3087e83fa9caeb3171dad47549
YujeongLEE <dldbwjd24@ewhain.net>
2023-08-17 21:03:02 +0900

    chore : (premium check)error명 수정

--- src/main/java/mathrone/backend/repository/SubscriptionRepository.java
+++ src/main/java/mathrone/backend/repository/SubscriptionRepository.java
@@ -17,5 +17,9 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
 
     //구독이 (완료된것만) complete 상태이며, 가장 최신의 것(가장 최근 구독내역으로)
     @Query(value = "select * from subscription where user_id=:userId and status = 'COMPLETE' order by modified_date desc limit 1", nativeQuery = true)
+    Optional<Subscription> checkLastSubscription(int userId);
+
+
+
     Optional<Subscription> findByUserId(int userId);
 }
