78331670668e6b0507d7a157637ed64ef58f7b10
OhHongGi <5ghdrl@naver.com>
2023-09-15 17:34:24 +0900

    chore(build): Apply pr review

--- src/main/java/mathrone/backend/controller/dto/ChangePasswordDto.java
+++ src/main/java/mathrone/backend/controller/dto/ChangePasswordDto.java
@@ -1,14 +1,11 @@
 package mathrone.backend.controller.dto;
 
-import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Setter
 public class ChangePasswordDto {
+
+    @NotBlank
     String newPassword;
 }
