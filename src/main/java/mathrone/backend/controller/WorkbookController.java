c7c1b7a999e6dced80f7086e21b213d090b934d1
ohhonggi <33611439+ohhonggi@users.noreply.github.com>
2023-10-22 18:58:21 +0900

    (MR-148) problem api refactoring (#26)
    * fix(AnswerServiceImpl): Handle errors that failed to find a problem in the database
    
    * chore(ErrorCode): Add the error code
    
    code
    - NOT_FOUND_PROBLEM
    
    * chore(ErrorCode): Apply correct PK to ChapterRepository
    
    * chore(Problem): Change the column type
    
    column
    - workbook
    - chapter
    
    * chore(Profile*): Move APIs to problemController
    
    ProfileController -> ProblemController
    
    * chore(WorkBookInfo): Change the column type
    
    List -> Set
    
    * refactor(Problem*): Unify the type of the problem request
    
    * chore(ProblemDto): Modify the instance variable
    
    * chore(ProblemDto): Modify method names and parameters
    
    * refactor(Problem*): Modify queries to sort by problem id
    
    * chore(Problem*): Modify some logic
    
    * refactor(Problem*): Rename problem columns
    
    level_of_diff -> level
    
    * chore(ProblemController): Modify the API
    
    /problem/try API
    Path-variable -> Request-Param (onlyIncorrect)
    
    * refactor(Workbook): Modify the logic to get workbook list
    
    Modify the class name
    bookItem -> WorkbookDto
    
    Modify the column type of the WorkbookDto
    Long star -> Boolean star

--- src/main/java/mathrone/backend/controller/WorkbookController.java
+++ src/main/java/mathrone/backend/controller/WorkbookController.java
@@ -14,7 +14,7 @@ import mathrone.backend.controller.dto.UserEvaluateLevelRequestDto;
 import mathrone.backend.controller.dto.UserWorkbookDataInterface;
 import mathrone.backend.controller.dto.interfaces.UserSolvedWorkbookResponseDtoInterface;
 import mathrone.backend.domain.bookContent;
-import mathrone.backend.domain.bookItem;
+import mathrone.backend.controller.dto.WorkbookDto;
 import mathrone.backend.service.WorkBookService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -41,14 +41,15 @@ public class WorkbookController {
 
     @GetMapping("/list")
     @ApiOperation(value = "workbook 조회", notes = "parameter에 따라 filtering된 workbook 리스트 가져오기")
-    public ResponseEntity<List<bookItem>> bookList(
+    public ResponseEntity<List<WorkbookDto>> bookList(
         @RequestParam(value = "publisher", required = false, defaultValue = "all") String publisher,
-        @RequestParam(value = "sortType", required = false, defaultValue = "star") String sortType,
+        @RequestParam(value = "sortType", required = false, defaultValue = "like") String sortType,
         @RequestParam(value = "category", required = false, defaultValue = "all") String category,
-        @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum) {
-
+        @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
+        HttpServletRequest request) {
         Pageable paging = PageRequest.of(pageNum - 1, 9, Sort.by("workbookId")); //page 0부터임!
-        return ResponseEntity.status(OK).body(workBookService.getBookList(paging, publisher, category, sortType));
+        return ResponseEntity.status(OK)
+            .body(workBookService.getBookList(request, paging, publisher, category, sortType));
     }
 
 
@@ -70,9 +71,10 @@ public class WorkbookController {
 
     @GetMapping("/")
     public ResponseEntity<BookDetailDto> workbookDetail(
-            @RequestParam(value = "id") String bookId) {
+        @RequestParam(value = "id") String bookId) {
         return ResponseEntity.status(OK).body(workBookService.getWorkbookDetail(bookId));
     }
+
     @GetMapping("/try")
     @ApiOperation(value = "사용자가 시도한 문제집 리스트 반환", notes = "access token가 존재하면 특정 사용자, 존재하지 않으면 모든 사용자가 시도한 문제집 리스트를 반환")
     public ResponseEntity<List<UserWorkbookDataInterface>> getTriedWorkbooks(
@@ -108,14 +110,16 @@ public class WorkbookController {
     public ResponseEntity<List<UserSolvedWorkbookResponseDtoInterface>> trackSolvedWorkbook(
         HttpServletRequest request,
         @PathVariable(value = "workbookId", required = false) Optional<String> workbookId) {
-        return ResponseEntity.status(OK).body(workBookService.trackSolvedWorkbook(request, workbookId));
+        return ResponseEntity.status(OK)
+            .body(workBookService.trackSolvedWorkbook(request, workbookId));
     }
 
     @PostMapping("/level")
     @ApiOperation(value = "유저의 문제집 평가", notes = "유저 token 필요")
     public ResponseEntity<Void> evaluateWorkbook(
-        HttpServletRequest request, @RequestBody UserEvaluateLevelRequestDto userEvaluateLevelRequestDto
-    ){
+        HttpServletRequest request,
+        @RequestBody UserEvaluateLevelRequestDto userEvaluateLevelRequestDto
+    ) {
         workBookService.evaluateWorkbook(request, userEvaluateLevelRequestDto);
         return ResponseEntity.status(CREATED).build();
     }
--- src/main/java/mathrone/backend/repository/ChapterRepository.java
+++ src/main/java/mathrone/backend/repository/ChapterRepository.java
@@ -1,11 +1,10 @@
 package mathrone.backend.repository;
 
-import java.util.List;
 import java.util.Optional;
 import mathrone.backend.domain.ChapterInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChapterRepository extends JpaRepository<ChapterInfo, Long> {
+public interface ChapterRepository extends JpaRepository<ChapterInfo, String> {
 
     Optional<ChapterInfo> findByChapterId(String chapterId);
 }
