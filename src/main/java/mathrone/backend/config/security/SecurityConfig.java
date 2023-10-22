8ac5e8d95e36511771bd624206fda4a66c4f402b
hyunwoo <ashyonu@gmail.com>
2023-10-22 18:34:34 +0900

    Chore: add Monitoring app

--- src/main/java/mathrone/backend/config/security/SecurityConfig.java
+++ src/main/java/mathrone/backend/config/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         "/oauth/**",
         "/subscribe/**",
         "/profile/**",
+        "/actuator/**",
+        "/metrics/**",
     };
 
     @Bean // user password 암호화
