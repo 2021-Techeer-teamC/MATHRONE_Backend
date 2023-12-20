6defe1a140c20d51ea540aa3553b0253638c8ba9
hyunwoo <ashyonu@gmail.com>
2023-12-20 23:55:14 +0900

    feat: add S3 uploading

---
+++ src/main/java/mathrone/backend/controller/dto/ChangeProfileDto.java
@@ -0,0 +1,25 @@
+package mathrone.backend.controller.dto;
+
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangeProfileDto {
+
+    private String accountId;               // 닉네임
+    private String profileImg;              // 이미지 링크
+    private MultipartFile profileImgFile;   // 이미지 파일
+    private String phoneNum;
+
+    public void setProfileImg(String profileImg) {
+        this.profileImg = profileImg;
+    }
+}   // 프로필 수정에 필요한 데이터
+
